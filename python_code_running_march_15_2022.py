a56e7ea567c99e2323821ce7044d4cb5b977fea1
Jayanth-Devarakonda17122001 <102537107+Jayanth-Devarakonda17122001@users.noreply.github.com>
2022-03-28 21:04:47 +0530

    Add files via upload

---
+++ python_code_running_march_15_2022.py
@@ -0,0 +1,35 @@
+##print('netzwerk')   #alt+3 comment and alt+4 for uncomment and ctrl+z to returm the deleted line and ctrl+a is to select all the stuff on the current screen
+##print('hello')
+##print('hi')
+##print('happy')
+
+
+
+1. python is open source language
+2. python is interpreter language
+2.1 python is called interpreterr language because it does not compile
+2.2 does not compile means it does not create pre memory
+2.3 does not create pre memory means memory will be created on run time
+3.4 create a memory on run time is called dynamic memory allocation
+2.5 anything that is dynamically alloted is what we called as scripting language
+2.6 scripting language can also be interpreter language
+
+3. memory management in python is carried out by python virtual memory
+3.1 python virtual memory is called as PRIVATE HEAP
+
+4. PYTHON IS called as unstructured python programming language
+4.1 unstructred programming language means writting the code without any template to be followed.
+
+5. python is automated garbage collection
+5.1 automated garbage collection means variable that is not in use will be removed from the memory
+
+6. python is case sensitive
+
+7. python is indent specific
+
+8. python is sequential program execution
+8.1 means it will read character by character or line by line
+8.2 this line by line execution is happening because of the concept of GIL
+8.3 GIL means global interpreted lock
+8.4 means when one line get executued next line will be locked
+8.5 because of this mechanism python will be slower in execution when compare to other language
